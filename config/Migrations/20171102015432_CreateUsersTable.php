6ced1c4415cc735ff10907f49dd8ede6a4de20f9
Cessare Julius <djcsarjulio1997@gmail.com>
2017-11-01 22:20:01 -0400

    finalizacion de la creacion de la migracion de la tabla users, con sus respectivas llaves foraneas

--- config/Migrations/20171102015432_CreateUsersTable.php
+++ config/Migrations/20171102015432_CreateUsersTable.php
@@ -12,5 +12,21 @@ class CreateUsersTable extends AbstractMigration
      */
     public function change()
     {
+        $table = $this->table('users');
+        $table->addColumn('role', 'enum', array('values' => 'admin, user'))
+              ->addColumn('cargo', 'string', array('limit' => 100))
+              ->addColumn('usuario', 'string', array('limit' => 100))
+              ->addColumn('clave', 'string')
+              ->addColumn('active', 'boolean')
+              ->addColumn('created', 'datetime')
+              ->addColumn('modified', 'datetime')
+              ->create();
+
+              $refTable = $this->table("users");
+              $refTable->addColumn("per_id", "integer", array("signed" => "disable"))
+                       ->addForeignKey("per_id", "personas", "id", array("delete" => "CASCADE", "update" => "NO_ACTION"))
+                       ->addColumn("tur_id", "integer", array("signed" => "disable"))
+                       ->addForeignKey("tur_id", "turnos", "id", array("delete" => "CASCADE", "update" => "NO_ACTION"))
+                       ->update();
     }
 }
